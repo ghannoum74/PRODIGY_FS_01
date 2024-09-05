6d550c684f062cc2e81424e93a2b3d4b061577d7
ghannoum74 <abdelrahmangh74@gmail.com>
2024-09-05 16:17:08 +0300

    remove the log out functionality

--- client/src/components/utils/SubmittForm.jsx
+++ client/src/components/utils/SubmittForm.jsx
@@ -1,39 +1,15 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import { useEffect, useState } from "react";
-import useLogout from "../../hooks/useLogout";
 import PropTypes from "prop-types";
 // import SuccessfullPage from "./SuccessfullPage";
 const SubmittForm = ({ caption, to, btn }) => {
-  const navigate = useNavigate();
-  const [logOut, setLogOut] = useState("");
-  const { logout } = useLogout();
-  //when btn == log out so we in the profilePage route so i should have only one button with fex 1 and color red
-  useEffect(() => {
-    if (btn === "LOG OUT") {
-      setLogOut("logOutBtn");
-    }
-  }, [btn]);
-  const handleButtonClick = () => {
-    if (btn === "Close") {
-      console.log();
-    }
-    if (btn === "LOG OUT") {
-      logout();
-      navigate("/");
-    }
-  };
   return (
     <>
       <div className="last-one">
         <Link to={to} className="switch-page">
           {caption}
         </Link>
-        <button
-          type="submit"
-          className={`switch-bttn ${logOut}`}
-          onClick={handleButtonClick}
-        >
+        <button type="submit" className="switch-bttn">
           {btn}
         </button>
       </div>
